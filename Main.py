119820b37e663c2595df09a0557108a71079e871
Dawson Thompson <thompsondawson20@gmail.com>
2021-02-16 15:53:39 -0500

    Continuing work on auto update function

--- Main.py
+++ Main.py
@@ -1,18 +1,18 @@
-from importlib import reload
+import os
+import sys
+import urllib.request
+
+#NOTE this application REQUIRES executable permissions. It needs to be able to run itself after an update is fetched.
 applicationSource = None #Change to download link from github
+currentDirectory = os.path.dirname(os.path.realpath(__file__))
 
 print("Hi! I'm the management script. Let me run the bot for you...")
 import botMain #We're gonna run the bot as a subprocess to the updater
-botMain = reload(botMain)
-
-while(1):
-    if botMain.runProgramUpdate == 1:
-        print('The bot has requested an update')
-        print(f'Updating from {applicationSource}...')
-        #run actual file download here
-        print('Update has completed! Rebooting bot...')
-        botMain.runProgramUpdate = 0
-        continue
-    else:
-        print('Bot process has terminated without requesting an update. Shutting down...')
-        break
+if botMain.runProgramUpdate == 1:
+    print('The bot has requested an update')
+    print(f'Updating from {applicationSource}...')
+    #run actual file download here
+    print('Update has completed! Rebooting bot...')
+    os.execv('/usr/bin/python3', ["\"" + '/usr/bin/python3' + "\""] + sys.argv) #Python path needed to be hard coded due to some issues with how the system changes the executable path
+else:
+    print('Bot process has terminated without requesting an update. Shutting down...')
