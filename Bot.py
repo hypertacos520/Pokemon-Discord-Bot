5cbd93b1a345f3b19b6972c3c946f6ce4e280921
Dawson Thompson <thompsondawson20@gmail.com>
2021-03-12 23:15:27 -0500

    added very early multiplayer code

--- Bot.py
+++ Bot.py
@@ -16,8 +16,10 @@ runProgramUpdate = 0
 #Import Pokemon Data
 pokemonDataSet = pd.read_csv(programDirectory + "/Resources/CSV/Pokemon.csv")
 pokemonMovesDataSet = pd.read_csv(programDirectory + "/Resources/CSV/Moves.csv")
+pokemonTypeEffectivenessDataSet = pd.read_csv(programDirectory + "/Resources/CSV/TypeEffectiveness.csv")
 pokemonData = pokemonDataSet[['Name', 'HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed', 'Type 1', 'Type 2']].values.tolist()
-pokemonMoves = pokemonMovesDataSet[['identifier', 'type_id', 'power', 'pp', 'accuracy', 'priority', 'damage_class_id']].values.tolist()
+pokemonMoves = pokemonMovesDataSet[['identifier', 'type_id', 'power', 'pp', 'accuracy', 'priority', 'damage_class_id']].values.tolist() 
+pokemonTypeEffectiveness = pokemonTypeEffectivenessDataSet[['damage_type_id', 'target_type_id' ,'damage_factor']].values.tolist()
 
 #Open and apply Discord Bot Token
 #MAKE SURE THE BOT TOKEN IS IN botToken.txt!!
@@ -38,6 +40,10 @@ def wordToEmoji(string):
         return '🟢'
     elif string == 'back':
         return '⬅'
+    elif string == 'check':
+        return '✔️'
+    elif string == 'x':
+        return '❌'
     else:
         return None
 
@@ -234,11 +240,16 @@ class Pokemon:
 
     def takeDamage(self, enemyPokemon, usedMove): #return 1 if super effective
         temp = self.CurrentHP
-        self.CurrentHP = int(self.CurrentHP - (((((2*enemyPokemon.Level)/5)+2*usedMove.Power*(enemyPokemon.Attack/self.Defense))/50)+2)*3)
+        typeMultiplyer = getTypeEffectiveness(convert_type_to_num(usedMove.Type), convert_type_to_num(enemyPokemon.TypeOne))
+        self.CurrentHP = int(self.CurrentHP - ((((((2*enemyPokemon.Level)/5)+2*usedMove.Power*(enemyPokemon.Attack/self.Defense))/50)+2)*3*typeMultiplyer))
         if self.CurrentHP < 0:
             self.CurrentHP = 0
         temp = temp - self.CurrentHP
         print(f'Dealt {temp} damage to {enemyPokemon.Name} using {usedMove.Name}.')
+        if typeMultiplyer == 0.5: #Not effective
+            return 2
+        if typeMultiplyer == 2: #Super effective
+            return 1
     
     def numericalInputToMove(self, moveNum): #converts integer to move number
         if moveNum == 1:
@@ -250,7 +261,7 @@ class Pokemon:
         elif moveNum == 4:
             return self.MoveFour
         else:
-            return None
+            return ""
 
 #Discord Bot Commands
 @client.event
@@ -262,21 +273,77 @@ async def on_ready():
 async def on_message(message):
     #Bot Message Functions
     def check(reaction, user):
-            return user == message.author
+            return not user.bot #user == message.author
     
+    #Displays a multiplayer user interface to get the players on each side
+    async def multiplayerGame():
+        Player1 = None
+        Player2 = None
+        while(1):
+            menu = await message.channel.send(f'Starting a new multiplayer game! Please select either {wordToEmoji("red")} or {wordToEmoji("blue")} to pick a side!\n{wordToEmoji("red")} **Player 1 -** {Player1}\n{wordToEmoji("blue")} **Player 2 -** {Player2}')
+            if(Player1 != None and Player2 != None):
+                menu2 = await message.channel.send('Ready to start?')
+                await menu2.add_reaction(wordToEmoji('check'))
+                await menu2.add_reaction(wordToEmoji('x'))
+                while (1):
+                    try:
+                        reaction, user = await client.wait_for('reaction_add', timeout=60.0, check=check) #wait for user to react
+                    except asyncio.TimeoutError:
+                        await menu.delete()
+                        await menu2.delete()
+                        return "", ""
+                    print("reaction detected!")
+                    if reaction.emoji != wordToEmoji('check') and reaction.emoji != wordToEmoji('x'): #fixes a bug where the bot responds to any emoji reaction
+                        continue
+                    else:
+                        break
+                if(reaction.emoji == wordToEmoji('check')):
+                    await menu.delete()
+                    await menu2.delete()
+                    return Player1, Player2
+                elif(reaction.emoji == wordToEmoji('x')):
+                    await menu.delete()
+                    await menu2.delete()
+                    return "", ""
+                    break
+            await menu.add_reaction(wordToEmoji('red'))
+            await menu.add_reaction(wordToEmoji('blue'))
+            while (1):
+                try:
+                    reaction, user = await client.wait_for('reaction_add', timeout=60.0, check=check) #wait for user to react
+                except asyncio.TimeoutError:
+                    await menu.delete()
+                    return "", ""
+                if reaction.emoji != wordToEmoji('red') and reaction.emoji != wordToEmoji('blue'): #fixes a bug where the bot responds to any emoji reaction
+                    continue
+                else:
+                    break
+            if(reaction.emoji == wordToEmoji('red')):
+                if(Player1 == user):
+                    Player1 = None
+                else:
+                    Player1 = user
+            elif(reaction.emoji == wordToEmoji('blue')):
+                if(Player2 == user):
+                    Player2 = None
+                else:
+                    Player2 = user
+            await menu.delete()
+
     #displays the user input and gets a players move decision
     #0 = ran from battle 1-4 = a move number
-    async def playerInput(userPokemon):
+    async def playerInput(userPokemon, location):
         while (1):
-            menu = await message.channel.send(f'What will {userPokemon.Name} do?\n{wordToEmoji("red")} - Attack\n{wordToEmoji("blue")} - Run')
+            menu = await location.send(f'What will {userPokemon.Name} do?\n{wordToEmoji("red")} - Attack\n{wordToEmoji("blue")} - Run')
             await menu.add_reaction(wordToEmoji('red'))
             await menu.add_reaction(wordToEmoji('blue'))
             while (1):
                 print('Users turn. Waiting for menu seleciton.')
-                try:
+                try: 
                     reaction, user = await client.wait_for('reaction_add', timeout=60.0, check=check) #wait for user to react
                 except asyncio.TimeoutError:
-                    return None
+                    await menu.delete()
+                    return ""
                 if reaction.emoji != wordToEmoji('red') and reaction.emoji != wordToEmoji('blue'): #fixes a bug where the bot responds to any emoji reaction
                     continue
                 else:
@@ -284,7 +351,7 @@ async def on_message(message):
             #Display attack options
             if reaction.emoji == wordToEmoji('red'):
                 await menu.delete()
-                menu = await message.channel.send(f'Attack:\n{wordToEmoji("red")} - **[{userPokemon.MoveOne.Type}]** {userPokemon.MoveOne.Name}\n{wordToEmoji("blue")} - **[{userPokemon.MoveTwo.Type}]** {userPokemon.MoveTwo.Name}\n{wordToEmoji("yellow")} - **[{userPokemon.MoveThree.Type}]** {userPokemon.MoveThree.Name}\n{wordToEmoji("green")} - **[{userPokemon.MoveFour.Type}]** {userPokemon.MoveFour.Name}')
+                menu = await location.send(f'Attack:\n{wordToEmoji("red")} - **[{userPokemon.MoveOne.Type}]** {userPokemon.MoveOne.Name}\n{wordToEmoji("blue")} - **[{userPokemon.MoveTwo.Type}]** {userPokemon.MoveTwo.Name}\n{wordToEmoji("yellow")} - **[{userPokemon.MoveThree.Type}]** {userPokemon.MoveThree.Name}\n{wordToEmoji("green")} - **[{userPokemon.MoveFour.Type}]** {userPokemon.MoveFour.Name}')
                 await menu.add_reaction(wordToEmoji('back'))
                 await menu.add_reaction(wordToEmoji('red'))
                 await menu.add_reaction(wordToEmoji('blue'))
@@ -295,7 +362,8 @@ async def on_message(message):
                     try: 
                         reaction, user = await client.wait_for('reaction_add', timeout=60.0, check=check) #wait for user to react
                     except asyncio.TimeoutError:
-                        return None
+                        await menu.delete()
+                        return ""
                     if reaction.emoji != wordToEmoji('back') and reaction.emoji != wordToEmoji('red') and reaction.emoji != wordToEmoji('blue') and reaction.emoji != wordToEmoji('yellow') and reaction.emoji != wordToEmoji('green'): #fixes a bug where the bot responds to any emoji reaction
                         continue
                     else:
@@ -321,10 +389,9 @@ async def on_message(message):
             #run menu option
             elif reaction.emoji == wordToEmoji('blue'):
                 await menu.delete()
-                await message.channel.send('You got away safely.')
                 return 0 #Battle ends
             else: #This happens if the user is idle and does not select anything.
-                return None
+                return ""
     
     def runMoveSelection(userPokemon, userMoveSelection, enemyPokemon, enemyMoveSelection):
         userMove = userPokemon.numericalInputToMove(userMoveSelection)
@@ -348,8 +415,9 @@ async def on_message(message):
             if dealDamage == 2: #not effective if function returns 2
                 discordOutput = discordOutput + 'Its not very effective...\n'
 
-        if userMove == None:
-            return None
+        if userMove == "":
+            print(userMove)
+            return ""
 
         if userMove.Priority > enemyMove.Priority: #User attacks first
             performUserMove()
@@ -393,9 +461,11 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    global discordOutput
+
     if message.content.startswith('!pkmn'): 
         #Random encounter battle loop
-        if message.content.startswith('!pkmn wBattle'):
+        if message.content.startswith('!pkmn wEncounter'):
             #battle initial setup
             isInBattle = 1
             baseLevel = random.randint(3, 98) #level is generated between 3 and 98 because the real range is baseLevel +- up to 2
@@ -406,11 +476,11 @@ async def on_message(message):
             combatContext = await message.channel.send(f"Running through the tall grass...\nA wild {enemyPokemon.Name} has appeared!\nGo {userPokemon.Name}!\n**                                                                       {enemyPokemon.Name} [HP: {enemyPokemon.CurrentHP}/{enemyPokemon.TotalHP} | Lvl: {enemyPokemon.Level}]\n[HP: {userPokemon.CurrentHP}/{userPokemon.TotalHP} | Lvl: {userPokemon.Level}] {userPokemon.Name}**")
             #main battle loop
             while isInBattle:
-                global discordOutput
                 discordOutput = "" #All info the bot needs to send to discord should be stored in this variable
-                playerDecision = await playerInput(userPokemon)
+                playerDecision = await playerInput(userPokemon, message.channel)
                 if playerDecision == 0:
                     isInBattle = 0
+                    await message.channel.send('You got away safely.')
                     print('Battle has ended!')
                     break
                 randomEnemyMove = random.randint(1, 4) #Randomly decides on a move for the enemy to use
@@ -418,7 +488,7 @@ async def on_message(message):
                 status = f"**                                                                       {enemyPokemon.Name} [HP: {enemyPokemon.CurrentHP}/{enemyPokemon.TotalHP} | Lvl: {enemyPokemon.Level}]\n[HP: {userPokemon.CurrentHP}/{userPokemon.TotalHP} | Lvl: {userPokemon.Level}] {userPokemon.Name}**"
                 discordOutput = discordOutput + status
                 await combatContext.delete()
-                if result == None:
+                if result == "":
                     discordOutput = discordOutput + '\nUser was idle for too long! Battle has ended.'
                     await message.channel.send(discordOutput)
                     isInBattle = 0
@@ -439,6 +509,79 @@ async def on_message(message):
                 combatContext = await message.channel.send(discordOutput)
                 continue
 
+        #Beta Multiplayer Battles
+        elif message.content.startswith('!pkmn pBattle'):
+            while(1):
+                #battle initial setup
+                Player1 = None
+                Player2 = None
+                Player1, Player2 = await multiplayerGame()
+                if(Player1 == "" and Player2 == ""):
+                    await message.channel.send('Game setup has been terminated.')
+                    break
+                isInBattle = 1
+                baseLevel = random.randint(3, 98) #level is generated between 3 and 98 because the real range is baseLevel +- up to 2
+                player1Pokemon = Pokemon()
+                player2Pokemon = Pokemon()
+                player1Pokemon.selectNewPokemon(baseLevel)
+                player2Pokemon.selectNewPokemon(baseLevel)
+                combatContext = await message.channel.send(f"{Player1} has challenged {Player2} to a Pokemon battle!\n{Player1} sends out {player1Pokemon.Name} and {Player2} sends out {player2Pokemon.Name}!\n**                                                                       {player2Pokemon.Name} [HP: {player2Pokemon.CurrentHP}/{player2Pokemon.TotalHP} | Lvl: {player2Pokemon.Level}]\n[HP: {player1Pokemon.CurrentHP}/{player1Pokemon.TotalHP} | Lvl: {player1Pokemon.Level}] {player1Pokemon.Name}**")
+                #main battle loop
+                #The main loop for multiplayer requires quite a bit of work
+                #Discord can check the reactions of messages in a server but not through DMs
+                #This is not ideal but the current solution is to have it send both player inputs to the server
+                while isInBattle:
+                    discordOutput = "" #All info the bot needs to send to discord should be stored in this variable
+                    #Get player 1 input
+                    p2message = await message.channel.send(f"It's {Player1}'s turn!") #change to Player2.send later
+                    while(1):
+                        #p1message = await Player1.send("It's your turn!")
+                        player1Decision = await playerInput(player1Pokemon, message.channel) #Change to Player1 in the future
+                        if player1Decision == 0:
+                            runMessage = await message.channel.send("You cannot run from a player battle!") #change to Player1.send later
+                            continue
+                        #await p1message.delete()
+                        await p2message.delete()
+                        await runMessage.delete()
+                        break
+                    #Get player 2 input
+                    p1message = await message.channel.send(f"It's {Player2}'s turn!") #change to Player1.send later
+                    while(1):
+                        #p2message = await Player2.send("It's your turn!")
+                        player2Decision = await playerInput(player2Pokemon, message.channel) #Change to Player2 in the future
+                        if player2Decision == 0:
+                            runMessage = await message.channel.send("You cannot run from a player battle!") #change to Player2.send later
+                            continue
+                        await p1message.delete()
+                        #await p2message.delete()
+                        runMessage.delete()
+                        break
+                    result = runMoveSelection(player1Pokemon, player1Decision, player2Pokemon, player2Decision)
+                    status = f"**                                                                       {player2Pokemon.Name} [HP: {player2Pokemon.CurrentHP}/{player2Pokemon.TotalHP} | Lvl: {player2Pokemon.Level}]\n[HP: {player1Pokemon.CurrentHP}/{player1Pokemon.TotalHP} | Lvl: {player1Pokemon.Level}] {player1Pokemon.Name}**"
+                    discordOutput = discordOutput + status
+                    await combatContext.delete()
+                    if result == "":
+                        discordOutput = discordOutput + '\nUser was idle for too long! Battle has ended.'
+                        await message.channel.send(discordOutput)
+                        isInBattle = 0
+                        print('Battle has ended!')
+                        break
+                    if result == 0:
+                        discordOutput = discordOutput + f'\n{Player2} Wins!'
+                        await message.channel.send(discordOutput)
+                        isInBattle = 0
+                        print('Battle has ended!')
+                        break
+                    if result == 1:
+                        discordOutput = discordOutput + f'\n{Player1} Wins!'
+                        await message.channel.send(discordOutput)
+                        isInBattle = 0
+                        print('Battle has ended!')
+                        break
+                    combatContext = await message.channel.send(discordOutput)
+                    continue
+                break
+
         #Run auto update system
         elif message.content.startswith('!pkmn Update'):
             await message.channel.send('Updating from source and restarting...') 
@@ -448,6 +591,6 @@ async def on_message(message):
 
         #Default response when a command is not detected
         else:
-            await message.channel.send('Unrecognized command! The available options are:\n\n wBattle --Start a fully randomized Wild Pokemon Encounter\n Update --Update bot from source and restart') #\n- lookup --Pull up the data entry for a given Pokemon
+            await message.channel.send('Unrecognized command! The available options are:\n\n wEncounter --Start a fully randomized Wild Pokemon Encounter\n pBattle --Start a fully randomized PvP battle\n Update --Update bot from source and restart') #\n- lookup --Pull up the data entry for a given Pokemon
 
 client.run(botToken) #Discord bot token. Now applied externally via botToken.txt
